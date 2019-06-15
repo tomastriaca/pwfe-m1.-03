51ffe76a60801e403e8bb0835e52e1100b78a6db
--global <--global>
2019-06-14 21:09:16 -0300

    agregando puertos dinamicos para heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 
 //const port = PUERTO_DE_HEROKU ||PUERTO_MIO
-const port = process.env.Port || 80
+const port = process.env.PORT|| 80
 http.createServer((request, response) => {
 		
 		let dir = "public/";
